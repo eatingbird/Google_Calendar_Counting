61e6c8d9b8ae0382e7699ed0a221030afadf33df
eatingbird <eatingbird@users.noreply.github.com>
2016-09-29 15:11:40 -0700

    An example on the Python standard library

---
+++ Use_Of_standard_library.py
@@ -0,0 +1,14 @@
+import sys
+
+class Foo(object):
+    pass
+
+def print_classes():
+    current_module = sys.modules[__name__]
+    for key in dir(current_module):
+        if isinstance( getattr(current_module, key), type ):
+            print(key)
+
+# test.py
+import foo
+foo.print_classes()
